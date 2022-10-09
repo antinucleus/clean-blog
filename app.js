c90640b58ae4449ce83d3560560501382688543b
antinucleus <antinucleuss@gmail.com>
2022-10-09 16:15:25 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const app = () => {
-  console.log('Hello')
-}
+const express = require('express')
+const path = require('path')
 
-app()
+const app = express()
+
+app.use(express.static('public'))
+
+const PORT = 3001
+
+app.get('/', (req, rep) => {
+  rep.sendFile(path.resolve(__dirname, 'temp/index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+})
