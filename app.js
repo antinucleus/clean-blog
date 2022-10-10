6c6d359f600f69af5c2424380f3ece263d544f96
antinucleus <antinucleuss@gmail.com>
2022-10-10 21:18:50 +0300

    feat: Add docker-compose file. feat: Add Post model. feat: Add mongoose package. fix: add_post and index file that user can be add new post and immediately see it in main page.

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+const Post = require('./models/Post')
 
 const app = express()
+
+const DB_ADDRESS = 'mongodb://localhost:27017/'
+const DB_NAME = 'cleanblog-test-db'
+
+const uri = `${DB_ADDRESS}${DB_NAME}`
+
+mongoose.connect(uri, (error) => {
+  if (error) {
+    console.log('Error occured while connecting to DB :\n', error)
+  } else {
+    console.log('Database connection is successful\n')
+  }
+})
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 const PORT = 3001
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', async (req, res) => {
+  const posts = await Post.find({})
+
+  res.render('index', { posts })
 })
 
 app.get('/about', (req, res) => {
@@ -18,6 +38,13 @@ app.get('/about', (req, res) => {
 
 app.get('/add_post', (req, res) => {
   res.render('add_post')
+})
+
+app.post('/add_post', async (req, res) => {
+  const { body } = req
+
+  await Post.create(body)
+  res.redirect('/')
 })
 
 app.listen(PORT, () => {
